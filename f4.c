ec8483cfb5ca853cb4635175b1a1bebef4c83a86
Ixinite <cmetahka2002@mail.ru>
2022-05-18 18:21:57 +0300

    basa kormit

--- f4.c
+++ f4.c
@@ -3,24 +3,25 @@
 
 #define VERTEX_COUNT 100
 
-void swap(int* xp, int* yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
 
-
-void bubbleSort(int arr[], int n, int **secondArr)
+void insertSort(int arr[], int n, int unsorted_vertex[])
 {
-    int i, j;
-    for (i = 0; i < n - 1; i++)
+    int i, j, temp, temp_i;
+    for (i = 1; i < n; i++)
     {
-        for (j = 0; j < n - i - 1; j++)
-            if (arr[j] > arr[j + 1])
-                swap(&arr[j], &arr[j + 1]);
-    }
-                
+        temp = arr[i];
+        temp_i = unsorted_vertex[i];
+        for (j = i - 1; j >= 0; j--)
+        {
+            if (arr[j] < temp)
+                break;
+    
+            arr[j + 1] = arr[j];
+            arr[j] = temp;
+            unsorted_vertex[j + 1] = unsorted_vertex[j];
+            unsorted_vertex[j] = temp_i;
+        }
+    }             
 }
 
 int weight_count(int *line, int len, int i)
@@ -41,6 +42,7 @@ int main(void)
     int maxlen = VERTEX_COUNT*10;
     char line[maxlen];
     int **matrix;
+    int *output_vertex;
 
     int w = 0, h = 0, cc = 0, len;
     char input[VERTEX_COUNT];
@@ -52,12 +54,12 @@ int main(void)
     FILE * matrixFile;
 
 
+    /*   ------------------             СОЗДАЕМ ДВУМЕРНЫЙ МАССИВ ДЛЯ ГРАФА               --------------    */
     matrix = (int**) malloc(VERTEX_COUNT * sizeof(int*));
     for(int i = 0; i < VERTEX_COUNT; i++){
         matrix[i] = (int*) malloc(VERTEX_COUNT * sizeof(int));
     }
     
-
 
     /*   ------------------             СЧИТЫВАЕТ ФАЙЛИК С МАТРИЦЕЙ               --------------    */
     matrixFile = fopen("matrix.txt", "r");
@@ -80,7 +82,9 @@ int main(void)
                     matrix[h][w] = 0;
                 else
                     matrix[h][w] = 1;
-                printf("%d-%d:%d\n", h+1, w+1, matrix[h][w]);
+
+                if(matrix[h][w]) printf("%d-%d\n", h+1, w+1);       //Выводит, если есть грань
+
                 w++;
             }
         }
@@ -91,11 +95,23 @@ int main(void)
     len = h;
     vertex_weight = (int *) malloc(len * sizeof(int));
 
-    printf("What edge to delete? Enter like 'x y': ");
-    scanf("%d %d", &removal_edge[0], &removal_edge[1]);
-    printf("%d-%d", removal_edge[0], removal_edge[1]);
+
+    /*   ------------------             СОЗДАЕМ МАССИВ С НУМЕРАЦИЕЙ ВЕРШИН               --------------    */
+    output_vertex = (int*) malloc(len * sizeof(int));
+    for(int i = 0; i < len; i++)
+    {
+        output_vertex[i] = i + 1;
+    }
+
 
     /*   ------------------             УДАЛЯЕМ ГРАНЬ                --------------    */
+    printf("What edge to delete? Enter like 'x y': ");
+    scanf("%d %d", &removal_edge[0], &removal_edge[1]);
+
+    if(!matrix[removal_edge[0] - 1][removal_edge[1] - 1]){
+        printf("You pick couple of vertex that haven't edge or oversize.\n");
+		return 1;
+    }
     matrix[removal_edge[0] - 1][removal_edge[1] - 1] = 0;
     matrix[removal_edge[1] - 1][removal_edge[0] - 1] = 0;
 
@@ -104,20 +120,20 @@ int main(void)
     for(int i = 0; i < len; i++)
     {
         vertex_weight[i] = weight_count(matrix[i], len, i);
-        printf("%d", vertex_weight[i]);
     }
 
-    /*   ------------------             СОРТИРОВКА БУБЛИКОМ               --------------    */
-    printf("\n");
-    bubbleSort(vertex_weight, len, matrix);
-    printf("\n");
+    /*   ------------------             СОРТИРОВКА ВСТАВКАМИ               --------------    */
+
+    insertSort(vertex_weight, len, output_vertex);
+
 
     /*   --------------------           ТУПО ВЫВОД              ----------------------   */
+    printf("Sorted Vertex by weight\n");
     for(int j = 0; j< h; j++){
-        for(int k = 0; k < w; k++){
-            printf("%d-%d: %d\n", j+1, k+1, matrix[j][k]);
-        }
-        //printf("%d ", vertex_weight[j]);
+        // for(int k = 0; k < w; k++){
+        //     printf("%d-%d: %d\n", j+1, k+1, matrix[j][k]);
+        // }
+        printf("%d ", output_vertex[j]);
     }
 
     return 1;
